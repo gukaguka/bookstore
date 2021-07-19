9286321ec21cfe97bedc422cd74be5806cae147b
unknown <guka.gogichaishvilI@gmail.com>
2021-07-19 19:14:10 +0400

    first commit

--- backend/models/signupmodels.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const schema = new mongoose.Schema({
-	name: {type: String, require: true},
-	phone: {type: String, require: true},
-})
-
-
-module.exports = mongoose.model('bookstoredb', schema);
